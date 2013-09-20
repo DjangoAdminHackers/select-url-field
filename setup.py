ca2276587ef30f82e35de40cb6c477e8186ca822
fruitschen <fruitschen@gmail.com>
2013-09-20 23:31:19 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import find_packages
 from setuptools import setup
 
-setup(name='newsletters',
+setup(name='ixxy-url-field',
       version='0.1',
       description='ixxy url field',
       author='Andy Baker',
       author_email='andy@andybak.net',
       packages=find_packages(),
       package_data={
-          'newsletters': [
+          'ixxy_url_field': [
             'static/admin/*',
           ]
       },
