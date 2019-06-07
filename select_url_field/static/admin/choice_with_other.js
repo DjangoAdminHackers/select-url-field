ecd3a4f2b9c56feeed3d37a0a34b08ae085307ce
Chen Zhe <fruitschen@gmail.com>
2019-06-08 00:54:12 +0800

    bug fix:  selecturl field dropdown do not work for new inline before they are saved.

--- select_url_field/static/admin/choice_with_other.js
+++ select_url_field/static/admin/choice_with_other.js
@@ -1,31 +1,35 @@
 (function($) {
     var OTHER_CHOICE = '__other__'; //See
     $(function(){
-        var wrappers = $('.choice_with_other_wrapper');
-        wrappers.each(function(){
-            var select = $('select', this);
-            var text_input = $('input', this);
-            select.change(function(){
-                if(select.val() == OTHER_CHOICE){
-                    ;
-                }else{
-                    text_input.val(select.val());
+        $('body').delegate('.choice_with_other_wrapper select', 'change', function(e){
+            var select = $(this)
+            var text_input = $(this).parent().find('input')
+            if(select.val() == OTHER_CHOICE){
+                ;
+            }else{
+                text_input.val(select.val());
+            }
+        });
+        var text_input = $('input', this);
+        $('body').delegate('.choice_with_other_wrapper input', 'keyup', function(e){
+            var match = false;
+            var text_input = $(this)
+            var select = $(this).parent().find('select')
+            $(select).find('option').each(function(){
+                if($(this).attr('value') == text_input.val()){
+                    match = true;
                 }
-            });
-            text_input.keyup(function(){
-                var match = false;
-                $(select).find('option').each(function(){
-                    if($(this).attr('value') == text_input.val()){
-                        match = true;
-                    }
-                })
-                if(!match){
-                    select.val(OTHER_CHOICE);
-                }else{
-                    select.val(text_input.val());
-                }
-            });
-            select.change();
+            })
+            if(!match){
+                select.val(OTHER_CHOICE);
+            }else{
+                select.val(text_input.val());
+            }
         })
+
+        $('.choice_with_other_wrapper').each(function(){
+            $('select', this).change();
+        })
+
     })
 })(django.jQuery);
